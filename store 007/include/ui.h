21b5033bb90dae0c484252e5e706342f7bad18d4
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2026-09-17 10:00:23 +0300

    Add files via upload

---
+++ store 007/include/ui.h
@@ -0,0 +1,10 @@
+//ui.h
+#ifndef UI_H
+#define UI_H
+
+#include <gtk/gtk.h>
+
+void ui_window_show(int argc, char *argv[]);
+void ui_about_dialog_show(GtkWindow *parent);
+
+#endif
---
+++ store 007/src/ui_about.c
@@ -0,0 +1,23 @@
+//ui_about.c
+#include "ui.h"
+#include "icon_manager.h"
+
+void ui_about_dialog_show(GtkWindow *parent) {
+    GtkWidget *dialog = gtk_about_dialog_new();
+    gtk_window_set_transient_for(GTK_WINDOW(dialog), parent);
+    gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
+
+    gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(dialog), "Helwan Software Store");
+    gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(dialog), "5.0");
+    gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dialog), "Professional Package Center for Helwan Linux");
+    gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(dialog), "Copyright © 2026 Helwan Linux");
+    
+    GdkPixbuf *logo = icon_manager_load_about_logo("data/about-logo.png");
+    if (logo) {
+        gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(dialog), logo);
+        g_object_unref(logo);
+    }
+
+    g_signal_connect(dialog, "response", G_CALLBACK(gtk_widget_destroy), NULL);
+    gtk_widget_show_all(dialog);
+}
